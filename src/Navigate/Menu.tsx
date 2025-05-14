e4b73a21a605daa67a9d414906e36ae631e08693
12ed3e34dw <pvlad8056@gmail.com>
2025-05-14 03:19:37 +0300

    Start

--- src/Navigate/Menu.tsx
+++ src/Navigate/Menu.tsx
@@ -4,70 +4,81 @@ import { useEffect, useState } from 'react';
 
 
 //foto Menu
-import generate_foto from "";
-import history_foto from "";
-import scanner_foto from "";
 
 
 
 export default function App() {
 
 return (
-    <View style={styles.container}>
+  <View style={styles.container}>
 
-<TouchableOpacity>
-    <View style={styles.container_1}>
-
+<TouchableOpacity onPress={() => {}}>
+    <View style={styles.generate_container}>
+        <Text style={styles.Text_Generate}>Generate</Text>
     </View>
 </TouchableOpacity>
 
+      <TouchableOpacity onPress={() => {}}>
+          <View style={styles.scanner_container}></View>
+      </TouchableOpacity>
+
+      <TouchableOpacity onPress={() => {}}>
+          <View style={styles.history_container}>
+              <Text style={styles.Text_History}>History</Text>
+          </View>
+      </TouchableOpacity>
+  </View>
 
 
-
-
-
-        <TouchableOpacity>
-            <Text style={styles.Text_Histiry}>History</Text>
-        </TouchableOpacity>
-
-
-        <TouchableOpacity>
-
-        </TouchableOpacity>
-
-        <TouchableOpacity>
-            <Text style={styles.Text_Generate}>Generate</Text>
-        </TouchableOpacity>
-
-    </View>
 )
 }
 
 const styles = StyleSheet.create({
-    container: {
-        top:660,
-        width:300,
-        left:40,
-        height:80,
+    container: {},
+
+    generate_container: {
+        position: 'absolute',
+        width: 150,
+        height: 65,
+        left: 20,
+        top: 630,
         backgroundColor: '#333333',
-        borderRadius:5,
+        zIndex: 4,
     },
-    Text_Generate: {
-        left:25,
-        top:-50,
-        color:'white',
-    },
-    Text_Histiry: {
-        color:'white',
-        left:220,
-        top:-30,
-    },
-container_1: {
+
+    scanner_container: {
+        position: 'absolute',
+        left: 130,
+        top: 590,
+        width: 70,
+        height: 70,
         backgroundColor: '#FDB623',
-    width:70,
-    height:70,
-    top:-40,
-    left:115,
-    borderRadius:45,
-},
-})
+        borderRadius: 100,
+        zIndex: 1, // выше остальных
+    },
+
+    history_container: {
+        position: 'absolute',
+        top: 630,
+        left: 170,
+        width: 180,
+        height: 65,
+        backgroundColor: '#333333',
+        zIndex: 4,
+
+    },
+
+    Text_Generate: {
+        color: 'white',
+        top: 30,
+        left: 40,
+    },
+
+    Text_History: {
+        top: 20,
+        left: 40,
+        color: 'white',
+    },
+});
+
+

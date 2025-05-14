e1e78399d4ad8689c93f0a9e6834fafa6b027c60
12ed3e34dw <pvlad8056@gmail.com>
2025-05-14 17:16:52 +0300

    Start

--- src/Navigate/Menu.tsx
+++ src/Navigate/Menu.tsx
@@ -1,84 +1,96 @@
+import {StyleSheet, View, Text, TouchableOpacity, Image} from 'react-native';
 import { StatusBar } from 'expo-status-bar';
-import {StyleSheet, View, Text, TouchableOpacity} from 'react-native';
-import { useEffect, useState } from 'react';
 
 
-//foto Menu
+//foto Navigate
+import foto_Scanner from '../../src/assets/assets_Navigate/foto_Scanner.png';
+import foto_generate from '../../src/assets/assets_Navigate/foto_generate.png';
+import foto_history from '../../src/assets/assets_Navigate/foto_history.png';
 
 
 
-export default function App() {
-
-return (
-  <View style={styles.container}>
-
-<TouchableOpacity onPress={() => {}}>
-    <View style={styles.generate_container}>
-        <Text style={styles.Text_Generate}>Generate</Text>
+export default function Menu() {
+    return (
+        <View style={styles.container}>
+<View style={styles.container_1}>
+    <TouchableOpacity>
+        <View style={styles.container_history}>
+            <Image source={foto_history} style={styles.Img_history} />
+        <Text style={styles.Text_history}>History</Text>
+        </View>
+    </TouchableOpacity>
+    <View style={styles.container_scanner}>
+        <TouchableOpacity>
+<Image source={foto_Scanner} style={styles.Img_Scanner}/>
+        </TouchableOpacity>
     </View>
-</TouchableOpacity>
-
-      <TouchableOpacity onPress={() => {}}>
-          <View style={styles.scanner_container}></View>
-      </TouchableOpacity>
-
-      <TouchableOpacity onPress={() => {}}>
-          <View style={styles.history_container}>
-              <Text style={styles.Text_History}>History</Text>
-          </View>
-      </TouchableOpacity>
-  </View>
-
-
-)
+    <TouchableOpacity>
+        <View style={styles.container_generate}>
+<Image source={foto_generate} style={styles.Img_generate}/>
+        <Text style={styles.Text_generate}>Generate</Text>
+        </View>
+    </TouchableOpacity>
+</View>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-    container: {},
+   container: {},
 
-    generate_container: {
-        position: 'absolute',
-        width: 150,
-        height: 65,
-        left: 20,
-        top: 630,
-        backgroundColor: '#333333',
-        zIndex: 4,
+    Text_generate: {
+        color:'white',
+        left:30,
+        top:13,
     },
+    Text_history: {
+       left:64,
+        top:13,
+        color:'white',
+    },
+    container_1: {
 
-    scanner_container: {
-        position: 'absolute',
-        left: 130,
-        top: 590,
-        width: 70,
-        height: 70,
+    },
+   container_history: {
+       width:160,
+       height:70,
+       backgroundColor:'#333333',
+       top:670,
+       left:180,
+
+   },
+    container_generate: {
+       width:150,
+        height:70,
+        backgroundColor: '#333333',
+        top:550,
+        left:30,
+    },
+    container_scanner: {
+       width:50,
+        height:50,
         backgroundColor: '#FDB623',
-        borderRadius: 100,
-        zIndex: 1, // выше остальных
+        left:155,
+        borderRadius: 30,
+top:580,
+        zIndex:9999,
     },
-
-    history_container: {
-        position: 'absolute',
-        top: 630,
-        left: 170,
-        width: 180,
-        height: 65,
-        backgroundColor: '#333333',
-        zIndex: 4,
-
+    Img_Scanner: {
+       width:70,
+        height:80,
+        left:-10,
+        top:-15.5,
     },
-
-    Text_Generate: {
-        color: 'white',
-        top: 30,
-        left: 40,
+    Img_generate: {
+       width:30,
+        height:30,
+       left:40,
+        top:8,
     },
-
-    Text_History: {
-        top: 20,
-        left: 40,
-        color: 'white',
+    Img_history: {
+       width:30,
+        top:5,
+        height:30,
+        left:70,
     },
 });
-
-

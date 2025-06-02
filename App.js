2c026b7dab48c0dd90a123db4e199a5de9ed57ba
12ed3e34dw <pvlad8056@gmail.com>
2025-06-02 14:45:01 +0300

    init

--- App.js
+++ App.js
@@ -4,8 +4,8 @@ import { useEffect, useState } from 'react';
 import {loadLanguage, changeLanguage} from './src/screens/Settings/i18n';
 import {useTranslation} from "react-i18next";
 // Splash Screens
-import Splash_1 from '@splash/Splash_Screen_active/Splash_Screen_Page_1';
-import Splash_2 from '@splash/Splash_Screen_active/Splash_Screen_Page_2';
+import Splash_1 from './src/screens/Splash_Screen/Splash_Screen_active/Splash_Screen_Page_1';
+import Splash_2 from './src/screens/Splash_Screen/Splash_Screen_active/Splash_Screen_Page_2';
 
 //Scanner
 import Scanner from './src/components/Scanners/Scanner';
